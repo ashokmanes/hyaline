d84775e29e3a97e054c532440d3f288e1ddc9880
ashokmanes <ashokmanes@gmail.com>
2020-05-24 17:21:09 +0530

    icon

--- src/index.js
+++ src/index.js
@@ -10,6 +10,16 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const hamburgerMenu = document.querySelector("#hamburger");
     const navUl = document.querySelector(".nav-ul");
 
+    const menuLink = document.querySelectorAll('.menu-link');
+
+    menuLink.forEach((link) => {
+        link.addEventListener('click', () => {
+            navUl.classList.toggle('show');
+            hamburgerMenu.classList.toggle('icon-toggle');
+        })
+    })
+    const year = document.querySelector("#copyyear");
+    year.innerHTML = new Date().getFullYear();
     hamburgerMenu.addEventListener('click', () => {
         navUl.classList.toggle('show');
         hamburgerMenu.classList.toggle('icon-toggle');
